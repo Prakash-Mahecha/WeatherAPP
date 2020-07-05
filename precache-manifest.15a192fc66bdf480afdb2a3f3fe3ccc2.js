33ffca4b9f17b5d6602711c93d53f9fb5d0afb88
prakash <prakash.18jics154@jietjodhpur.ac.in>
2020-07-05 15:42:13 +0530

    Updates

---
+++ precache-manifest.15a192fc66bdf480afdb2a3f3fe3ccc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1455791e5db9b6c977f661e8aab53de5",
+    "url": "/WeatherAPP/index.html"
+  },
+  {
+    "revision": "a4aed9182b75369f6df8",
+    "url": "/WeatherAPP/static/css/main.f07dba28.chunk.css"
+  },
+  {
+    "revision": "8b2a81a27130adadad74",
+    "url": "/WeatherAPP/static/js/2.921b4117.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherAPP/static/js/2.921b4117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4aed9182b75369f6df8",
+    "url": "/WeatherAPP/static/js/main.fd665977.chunk.js"
+  },
+  {
+    "revision": "f68628c1c5e788bbef20",
+    "url": "/WeatherAPP/static/js/runtime-main.880404ef.js"
+  }
+]);
